93b7ae4712dff064cc7950619b8340dca87f8e2b
Min Lu <luminwin@gmail.com>
2023-03-22 03:06:29 -0400

    Lu

--- docs/articles/b.js
+++ docs/articles/b.js
@@ -9,15 +9,15 @@ document.write(`
    
       <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-        <div class="Grid-cell"><div class="Demo">Want to impute missing values?<br/> <a href="https://www.randomforestsrc.org/reference/impute.rfsrc.html"><div class = "code">impute()</div> 
-</a>
+        <div class="Grid-cell"><div class="Demo">Want to impute missing values?<br/> <div class = "code"><a href="https://www.randomforestsrc.org/reference/impute.rfsrc.html">impute()
+</a></div> 
       
         </div></div>
       </div>   
             <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want to tune parameters?  <br/><a href="https://www.randomforestsrc.org/reference/tune.rfsrc.html"><div class = "code">tune()</div> 
-</a>
+              <div class="Grid-cell"><div class="Demo">Want to tune parameters?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/tune.rfsrc.html">tune()
+</a></div> 
       
         </div></div>
  
@@ -29,38 +29,43 @@ document.write(`
     <div class="Grid-cell "><div class="Grid-cell"><div class="Demo title-font">Fit the model</div></div><div class="Demo"> Choose one function to fit the model
       <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-        <div class="Grid-cell"><div class="Demo">If you don't have an outcome<br/> <a href="https://www.randomforestsrc.org/reference/sidclustering.rfsrc.html"><div class = "code">sidclustering()</div> 
-</a>
-      
+        <div class="Grid-cell"><div class="Demo">If you don't have an outcome:<br/> <div class = "code"><a href="https://www.randomforestsrc.org/reference/sidClustering.rfsrc.html">sidclustering()
+</a><a href="https://www.randomforestsrc.org/articles/sidClustering.html">-- Vignette</a></div> 
+   
         </div></div>
       </div>   
             <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want quantile regression?  <br/><div class = "code">quantreg()</div>
+              <div class="Grid-cell"><div class="Demo">Want quantile regression?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/quantreg.rfsrc.html">quantreg()
+</a></div> 
 </div></div>
 
       </div>
                  <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Have imbalanced regression?  <br/><div class = "code">imbalanced()</div>
+              <div class="Grid-cell"><div class="Demo">Want a class-imbalanced classifier?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/imbalanced.rfsrc.html">imbalanced()
+</a><a href="https://www.randomforestsrc.org/articles/imbalance.html">-- Vignette</a></div> 
 </div></div>
 
       </div> 
                  <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want to add synthetic features?  <br/><div class = "code">synthetic()</div>
+              <div class="Grid-cell"><div class="Demo">Want to add synthetic features?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/synthetic.rfsrc.html">synthetic()
+</a></div> 
 </div></div>
 
       </div> 
                  <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want fast computation for large data?  <br/><div class = "code">rfsc.fast()</div>
+              <div class="Grid-cell"><div class="Demo">Want fast computation for large data?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/rfsrc.fast.html">rfsc.fast()
+</a><a href="https://www.randomforestsrc.org/articles/speedup.html">-- Vignette</a></div> 
 </div></div>
 
       </div> 
                  <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo"> want to hide the data for safe transportation? <br/><div class = "code">rfsrc.anonymous</div>
+              <div class="Grid-cell"><div class="Demo"> Want to hide the data for safe transportation? <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/rfsrc.anonymous.html">rfsrc.anonymous()
+</a></div> 
 </div></div>
 
       </div> 
@@ -69,44 +74,52 @@ document.write(`
     <div class="arrow-right-long"></div>
     <div class="Grid-cell "><div class="Grid-cell"><div class="Demo title-font">Variable selection & Inference</div></div><div class="Demo">     
       <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
-        <div class="Grid-cell"><div class="Demo">Want the predicted outcome?<br/> <div class = "code">predict()</div>
+        <div class="Grid-cell"><div class="Demo">Want the predicted outcome?<br/> <div class = "code"><a href="https://www.randomforestsrc.org/reference/predict.rfsrc.html">predict()
+</a></div> 
       
         </div></div>
       </div>   
             <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want variable importance(VIMP)?  <br/><div class = "code">vimp()</div>
+              <div class="Grid-cell"><div class="Demo">Want variable importance(VIMP)?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/vimp.rfsrc.html">vimp()
+</a></div> 
       
         </div></div>
  
       </div> 
             <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want to obtain standard errors for VIMP?  <br/><div class = "code">subsample()</div>
+              <div class="Grid-cell"><div class="Demo">Want to obtain standard errors & confidence intervals for VIMP?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/subsample.rfsrc.html">subsample()
+</a><a href="https://www.randomforestsrc.org/articles/vimp.html">-- Vignette</a></div> 
       
         </div></div>
  
       </div>            <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want to find interactions?  <br/><div class = "code">find.interactions()</div>
+              <div class="Grid-cell"><div class="Demo">Want to find interactions?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/find.interaction.rfsrc.html">find.interactions()
+</a><a href="https://www.randomforestsrc.org/articles/minidep.html"><br/> Vignette</a></div> 
       
         </div></div>
  
       </div>            <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want to plot partial effect?  <br/><div class = "code">plot.variable()</div>
+              <div class="Grid-cell"><div class="Demo">Want to plot partial effect?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/plot.variable.rfsrc.html">plot.variable()
+</a><a href="https://www.randomforestsrc.org/articles/partial.html">-- Vignette</a></div> 
       
         </div></div>
  
       </div>            <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-              <div class="Grid-cell"><div class="Demo">Want more info of the trees?  <br/><div class = "code">get.tree() max.subtree()  stat.split()</div>
-      
+              <div class="Grid-cell"><div class="Demo">Want more info of the trees?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/get.tree.rfsrc.html">get.tree()
+</a><a href="https://www.randomforestsrc.org/reference/max.subtree.rfsrc.html">max.subtree()
+</a></a><a href="https://www.randomforestsrc.org/reference/stat.split.rfsrc.html">stat.split()
+</a></div> 
         </div></div>
  
       </div>            <div class="Grid Grid--gutters Grid--1of1 Grid--nested">
 
-   <div class="Grid-cell"><div class="Demo">Want fast var selection for large data?  <br/><div class = "code">var.select()</div>
+   <div class="Grid-cell"><div class="Demo">Want fast var selection for large data?  <br/><div class = "code"><a href="https://www.randomforestsrc.org/reference/var.select.rfsrc.html">var.select()
+</a></div>
       
         </div></div>
  
